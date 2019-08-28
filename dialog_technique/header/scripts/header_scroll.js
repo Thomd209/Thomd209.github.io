31334b404196d40b3f1fad0bf3be6cd4698f881d
Thomd209 <thomd209@gmail.com>
2019-08-28 23:22:45 +0300

    add updated files to project

--- dialog_technique/header/scripts/header_scroll.js
+++ dialog_technique/header/scripts/header_scroll.js
@@ -1,15 +1,15 @@
 $(window).scroll(function(){
     if(window.pageYOffset >= 100){
-        $('header').css("padding-bottom","4%");
+        $('header').css("padding-bottom","6%");
         $('header').css("transition","padding-bottom .5s");
         $('.header__logo').css("width", "10%");
         $('.header__logo').css("transition", "width .5s");
-        $('.header__menu').css("top", "20px");
+        $('.header__menu').css("top", "30px");
         $('.header__menu').css("transition", "top .5s");
         $('.header__contacts').css("display", "none");
     }
     else {
-        $('header').css("padding-bottom","8%");
+        $('header').css("padding-bottom","10%");
         $('header').css("transition","padding-bottom 1s");
         $('.header__logo').css("width", "20%");
         $('.header__logo').css("transition", "width 1s");
