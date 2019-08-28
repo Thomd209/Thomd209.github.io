35c833734f5be080603f735db628933890041f41
Thomd209 <thomd209@gmail.com>
2019-08-28 21:45:26 +0300

    add changed files to project

--- dialog_technique/header/scripts/header_scroll.js
+++ dialog_technique/header/scripts/header_scroll.js
@@ -1,7 +1,5 @@
 $(window).scroll(function(){
     if(window.pageYOffset >= 100){
-        $('header').css("height", "50px");
-        $('header').css("transition", "height .5s");
         $('.header__logo').css("width", "10%");
         $('.header__logo').css("transition", "width .5s");
         $('.header__menu').css("top", "20px");
@@ -9,11 +7,9 @@ $(window).scroll(function(){
         $('.header__contacts').css("display", "none");
     }
     else {
-        $('header').css("height", "100px");
-        $('header').css("transition", "height 1s");
         $('.header__logo').css("width", "20%");
         $('.header__logo').css("transition", "width 1s");
-        $('.header__menu').css("top", "45px");
+        $('.header__menu').css("top", "40%");
         $('.header__menu').css("transition", "top 1s");
         $('.header__contacts').css("display", "flex");
     }
