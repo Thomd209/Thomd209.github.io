69fcd632f0129b3d9a9bd77ba3cec5ad6f9a2802
Thomd209 <thomd209@gmail.com>
2019-09-12 19:54:43 +0300

    add new files and updated files to project

---
+++ dialog_technique/scripts/main-page__top-slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.top-slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider-partners').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+    });
+  });
