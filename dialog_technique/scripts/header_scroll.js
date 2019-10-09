3908ba38cd8ecdf140362ba3fd2b1db7f9819537
Thomd209 <thomd209@gmail.com>
2019-10-09 21:41:06 +0300

    add changed files to project dialog_technique

--- dialog_technique/scripts/header_scroll.js
+++ dialog_technique/scripts/header_scroll.js
@@ -1,26 +1,22 @@
 $(window).scroll(function(){
-    if(window.pageYOffset >= 100){
-        $('header').css("padding-bottom","6%");
-        $('header').css("transition","padding-bottom .5s");
+    if(window.pageYOffset >= 50){
         $('.header__logo').css("width", "10%");
         $('.header__logo').css("transition", "width .5s");
         $('.header__menu').css("top", "30px");
         $('.header__menu').css("transition", "top .5s");
-        $('.header__tel-number-bottom').css("top","60%");
-        $('.header__tel-number-bottom').css("transition","top 1s");
         $('.site-path').css("top","4.9em");
         $('.site-path').css("transition","top .5s");
+        $('header').css("padding-bottom","6%");
+        $('header').css("transition","padding-bottom .5s");
     }
     else {
-        $('header').css("padding-bottom","10%");
-        $('header').css("transition","padding-bottom .5s");
         $('.header__logo').css("width", "20%");
         $('.header__logo').css("transition", "width .5s");
         $('.header__menu').css("top", "40%");
         $('.header__menu').css("transition", "top 1s");
-        $('.header__tel-number-bottom').css("top","47%");
-        $('.header__tel-number-bottom').css("transition","top 1s");
-        $('.site-path').css("top","8em");
+        $('.site-path').css("top","8.3em");
         $('.site-path').css("transition","top .5s");
+        $('header').css("padding-bottom","10%");
+        $('header').css("transition","padding-bottom .5s");
     }
 });
