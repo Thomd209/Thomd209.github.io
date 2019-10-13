f0c8802dea1c18b17dfd9de7ef442e9c7a4c32b9
Thomd209 <thomd209@gmail.com>
2019-10-13 19:08:14 +0300

    add changed files to project

--- dialog_technique/scripts/main-page__top-slider.js
+++ dialog_technique/scripts/main-page__top-slider.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-    $('.top-slider').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
+  $('.top-slider').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
   });
+});
 
-  $(document).ready(function(){
-    $('.slider-partners').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: true,
-        autoplay: true,
-    });
+$(document).ready(function(){
+  $('.slider-partners').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      responsive: [
+        {
+          breakpoint: 980,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+        },
+        {
+          breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+        },
+      ]
   });
+});
