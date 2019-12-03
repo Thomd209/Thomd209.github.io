35365ecd760c2153c8c2d8eef457a39f2577ff9d
Thomd209 <thomd209@gmail.com>
2019-12-03 17:47:03 +0300

    add project ToDo_List to repository

---
+++ ToDo_List/app/scripts/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var tasks_list = document.querySelector('.app__tasks-list');
+var input_tasks = document.querySelector('.js-input-task');
+var create_task_btn = document.querySelector('.js-btn');
+
+create_task_btn.addEventListener('click', function () {
+    var appFooter = document.querySelector('.app__footer');
+    var tasksList = document.createElement('ul');
+    tasksList.className += 'app__tasks-list';
+    appFooter.append(tasksList);
+
+    var li = document.createElement('li');
+    li.className += 'app__list-item';
+    var span = document.createElement('span');
+    span.className += 'app__list-item-content';
+    span.innerHTML = input_tasks.value;
+    li.append(span);
+
+    var deleteBtn = document.createElement('span');
+    deleteBtn.className += 'app__delete-btn';
+    deleteBtn.innerHTML = '&otimes;';
+    li.append(deleteBtn);
+    tasksList.append(li);
+
+    var allTasks_info = document.querySelector('.js-all-tasks');
+    allTasks_info.innerHTML = +allTasks_info.innerHTML + 1;
+    span.addEventListener('click', doneTask);
+    deleteBtn.addEventListener('click', deleteTask);
+});
+
+input_tasks.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        var appFooter = document.querySelector('.app__footer');
+        var tasksList = document.createElement('ul');
+        tasksList.className += 'app__tasks-list';
+        appFooter.append(tasksList);
+
+        var li = document.createElement('li');
+        li.className += 'app__list-item';
+        var span = document.createElement('span');
+        span.className += 'app__list-item-content';
+        span.innerHTML = input_tasks.value;
+        li.append(span);
+
+        var deleteBtn = document.createElement('span');
+        deleteBtn.className += 'app__delete-btn';
+        deleteBtn.innerHTML = '&otimes;';
+        li.append(deleteBtn);
+        tasksList.append(li);
+
+        var allTasks_info = document.querySelector('.js-all-tasks');
+        allTasks_info.innerHTML = +allTasks_info.innerHTML + 1;
+        span.addEventListener('click', doneTask);
+        deleteBtn.addEventListener('click', deleteTask);
+    }
+});
+
+function doneTask() {
+    this.classList.toggle('doneTask');
+    var doneTasks = document.querySelector('.js-done-tasks');
+    if (this.classList.contains('doneTask')) {
+        doneTasks.innerHTML = +doneTasks.innerHTML + 1;
+    } else {
+        doneTasks.innerHTML = +doneTasks.innerHTML - 1;
+    }
+};
+
+function deleteTask() {
+    this.parentElement.style.display = 'none';
+    var allTasks_info = document.querySelector('.js-all-tasks');
+    allTasks_info.innerHTML = +allTasks_info.innerHTML - 1;
+    var doneTasks = document.querySelector('.js-done-tasks');
+    doneTasks.innerHTML = +doneTasks.innerHTML - 1;
+};
+
+var deleteAllTasks = document.querySelector('.js-delete-all-tasks');
+deleteAllTasks.addEventListener('click', function () {
+    var tasksList = document.querySelectorAll('.app__tasks-list');
+    tasksList.forEach(function (listItem) {
+        listItem.remove();
+    });
+
+    var allTasks_info = document.querySelector('.js-all-tasks');
+    allTasks_info.innerHTML = 0;
+    var doneTasks = document.querySelector('.js-done-tasks');
+    doneTasks.innerHTML = 0;
+});
