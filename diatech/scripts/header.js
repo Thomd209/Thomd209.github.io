0e4c05ebe5c88e9c2436e676cd7205c28b4b6c56
Thomd209 <thomd209@gmail.com>
2019-12-24 02:33:42 +0300

    change mobile-menu-icon

--- diatech/scripts/header.js
+++ diatech/scripts/header.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-    $('.header-mobile__menu-icon').click(function(){
-        $('.header-mobile__menu-all-links').toggleClass('header-mobile__menu-move-left');
-        $('.header-mobile__menu-all-links').css("transition","transform .5s");
-        $('html').toggleClass("no-scrolling");
-    });
+$('.header-mobile__menu-icon').click(function(){
+    this.classList.toggle("header-mobile__menu-icon-change");
+    this.children[0].classList.toggle("header-mobile__menu-icon-item-1-change");
+    this.children[0].style.transition = "transform .7s";
+    this.children[1].classList.toggle("header-mobile__menu-icon-item-2-change");
+    this.children[2].classList.toggle("header-mobile__menu-icon-item-3-change");
+    this.children[2].style.transition = "transform .7s";
+    $('.header-mobile__menu-all-links').toggleClass('header-mobile__menu-move-left');
+    $('.header-mobile__menu-all-links').css("transition","transform .5s");
+    $('html').toggleClass("no-scrolling");
 });
 
 $(window).scroll(function(){
