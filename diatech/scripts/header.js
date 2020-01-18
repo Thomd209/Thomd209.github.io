2cae442e83c0dd45d488e407be7bbbe75f204221
Thomd209 <thomd209@gmail.com>
2020-01-19 01:36:37 +0300

    Revert "add changes to project diatech"
    This reverts commit 0714adf49f92131612525981df561dedc2c5a89c.

--- diatech/scripts/header.js
+++ diatech/scripts/header.js
@@ -1,1 +1,38 @@
-$(".header-mobile__menu-icon").click(function(){this.classList.toggle("header-mobile__menu-icon-change"),this.children[0].classList.toggle("header-mobile__menu-icon-item-1-change"),this.children[0].style.transition="transform .7s",this.children[1].classList.toggle("header-mobile__menu-icon-item-2-change"),this.children[2].classList.toggle("header-mobile__menu-icon-item-3-change"),this.children[2].style.transition="transform .7s",$(".header-mobile__menu-all-links").toggleClass("header-mobile__menu-move-left"),$(".header-mobile__menu-all-links").css("transition","transform .5s"),$("html").toggleClass("no-scrolling")}),$(window).scroll(function(){50<=window.pageYOffset?($(".header-mobile__logo").css("width","7.5em"),$(".header-mobile__logo").css("transition",".5s"),$(".header-desktop__logo").css("width","10%"),$(".header-desktop__logo").css("transition","width .5s"),$(".header-desktop__menu").css("top","30px"),$(".header-desktop__menu").css("transition","top .5s"),$(".site-path").css("top","13.3%"),$(".site-path").css("transition","top .5s"),$(".header-desktop").css("padding-bottom","6%")):($(".header-mobile__logo").css("width","10em"),$(".header-mobile__logo").css("transition",".5s"),$(".header-desktop__logo").css("width","20%"),$(".header-desktop__logo").css("transition","width .5s"),$(".header-desktop__menu").css("top","40%"),$(".header-desktop__menu").css("transition","top .5s"),$(".site-path").css("top","9.3%"),$(".site-path").css("transition","top .5s"),$(".header-desktop").css("padding-bottom","10%")),$(".header-desktop").css("transition","padding-bottom .5s")});
+$('.header-mobile__menu-icon').click(function(){
+    this.classList.toggle("header-mobile__menu-icon-change");
+    this.children[0].classList.toggle("header-mobile__menu-icon-item-1-change");
+    this.children[0].style.transition = "transform .7s";
+    this.children[1].classList.toggle("header-mobile__menu-icon-item-2-change");
+    this.children[2].classList.toggle("header-mobile__menu-icon-item-3-change");
+    this.children[2].style.transition = "transform .7s";
+    $('.header-mobile__menu-all-links').toggleClass('header-mobile__menu-move-left');
+    $('.header-mobile__menu-all-links').css("transition","transform .5s");
+    $('html').toggleClass("no-scrolling");
+});
+
+$(window).scroll(function(){
+    if(window.pageYOffset >= 50){
+        $('.header-mobile__logo').css("width","7.5em");
+        $('.header-mobile__logo').css("transition",".5s");
+        $('.header-desktop__logo').css("width", "10%");
+        $('.header-desktop__logo').css("transition", "width .5s");
+        $('.header-desktop__menu').css("top", "30px");
+        $('.header-desktop__menu').css("transition", "top .5s");
+        $('.site-path').css("top","13.3%");
+        $('.site-path').css("transition","top .5s");
+        $('.header-desktop').css("padding-bottom","6%");
+        $('.header-desktop').css("transition","padding-bottom .5s");
+    }
+    else {
+        $('.header-mobile__logo').css("width","10em");
+        $('.header-mobile__logo').css("transition",".5s");
+        $('.header-desktop__logo').css("width", "20%");
+        $('.header-desktop__logo').css("transition", "width .5s");
+        $('.header-desktop__menu').css("top", "40%");
+        $('.header-desktop__menu').css("transition", "top .5s");
+        $('.site-path').css("top","9.3%");
+        $('.site-path').css("transition","top .5s");
+        $('.header-desktop').css("padding-bottom","10%");
+        $('.header-desktop').css("transition","padding-bottom .5s");
+    }
+});
--- diatech/scripts/main-page__top-slider.js
+++ diatech/scripts/main-page__top-slider.js
@@ -1,1 +1,36 @@
-$(document).ready(function(){$(".top-slider").slick({dots:!0,autoplay:!0,autoplaySpeed:3e3,prevArrow:"<span class='top-slider__prev-arrow'><i class='fas fa-chevron-left'></i></span>",nextArrow:"<span class='top-slider__next-arrow'><i class='fas fa-chevron-right'></i></span>"})}),$(document).ready(function(){$(".slider-partners").slick({slidesToShow:4,slidesToScroll:1,arrows:!0,autoplay:!0,prevArrow:"<span class='slider-partners__prev-arrow'><i class='fas fa-chevron-left'></i></span>",nextArrow:"<span class='slider-partners__next-arrow'><i class='fas fa-chevron-right'></i></span>",responsive:[{breakpoint:980,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:500,settings:{slidesToShow:1,slidesToScroll:1}}]})});
+$(document).ready(function(){
+  $('.top-slider').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow: "<span class='top-slider__prev-arrow'><i class='fas fa-chevron-left'></i></span>",
+      nextArrow: "<span class='top-slider__next-arrow'><i class='fas fa-chevron-right'></i></span>",
+  });
+});
+
+$(document).ready(function(){
+  $('.slider-partners').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      prevArrow: "<span class='slider-partners__prev-arrow'><i class='fas fa-chevron-left'></i></span>",
+      nextArrow: "<span class='slider-partners__next-arrow'><i class='fas fa-chevron-right'></i></span>",
+      responsive: [
+        {
+          breakpoint: 980,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+        },
+        {
+          breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+        },
+      ]
+  });
+});
